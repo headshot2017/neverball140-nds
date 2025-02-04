5b9ac69630a09cb80a559bc388174e539e7b00fe
headshot2017 <nou@jm>
2025-02-04 16:30:40 -0400

    f32 multiplication

--- source/share/vec3f.c
+++ source/share/vec3f.c
@@ -67,59 +67,59 @@ int  mf_inv(int *I, const int *M)
     int T[16];
     int d;
 
-    T[0] = +(M[5] * (M[A] * M[F] - M[B] * M[E]) -
-             M[9] * (M[6] * M[F] - M[7] * M[E]) +
-             M[D] * (M[6] * M[B] - M[7] * M[A]));
-    T[1] = -(M[4] * (M[A] * M[F] - M[B] * M[E]) -
-             M[8] * (M[6] * M[F] - M[7] * M[E]) +
-             M[C] * (M[6] * M[B] - M[7] * M[A]));
-    T[2] = +(M[4] * (M[9] * M[F] - M[B] * M[D]) -
-             M[8] * (M[5] * M[F] - M[7] * M[D]) +
-             M[C] * (M[5] * M[B] - M[7] * M[9]));
-    T[3] = -(M[4] * (M[9] * M[E] - M[A] * M[D]) -
-             M[8] * (M[5] * M[E] - M[6] * M[D]) +
-             M[C] * (M[5] * M[A] - M[6] * M[9]));
+    T[0] = +(mulf32(M[5], (mulf32(M[A], M[F]) - mulf32(M[B], M[E])) ) -
+             mulf32(M[9], (mulf32(M[6], M[F]) - mulf32(M[7], M[E])) ) +
+             mulf32(M[D], (mulf32(M[6], M[B]) - mulf32(M[7], M[A])) ));
+    T[1] = -(mulf32(M[4], (mulf32(M[A], M[F]) - mulf32(M[B], M[E])) ) -
+             mulf32(M[8], (mulf32(M[6], M[F]) - mulf32(M[7], M[E])) ) +
+             mulf32(M[C], (mulf32(M[6], M[B]) - mulf32(M[7], M[A])) ));
+    T[2] = +(mulf32(M[4], (mulf32(M[9], M[F]) - mulf32(M[B], M[D])) ) -
+             mulf32(M[8], (mulf32(M[5], M[F]) - mulf32(M[7], M[D])) ) +
+             mulf32(M[C], (mulf32(M[5], M[B]) - mulf32(M[7], M[9])) ));
+    T[3] = -(mulf32(M[4], (mulf32(M[9], M[E]) - mulf32(M[A], M[D])) ) -
+             mulf32(M[8], (mulf32(M[5], M[E]) - mulf32(M[6], M[D])) ) +
+             mulf32(M[C], (mulf32(M[5], M[A]) - mulf32(M[6], M[9])) ));
 
-    T[4] = -(M[1] * (M[A] * M[F] - M[B] * M[E]) -
-             M[9] * (M[2] * M[F] - M[3] * M[E]) +
-             M[D] * (M[2] * M[B] - M[3] * M[A]));
-    T[5] = +(M[0] * (M[A] * M[F] - M[B] * M[E]) -
-             M[8] * (M[2] * M[F] - M[3] * M[E]) +
-             M[C] * (M[2] * M[B] - M[3] * M[A]));
-    T[6] = -(M[0] * (M[9] * M[F] - M[B] * M[D]) -
-             M[8] * (M[1] * M[F] - M[3] * M[D]) +
-             M[C] * (M[1] * M[B] - M[3] * M[9]));
-    T[7] = +(M[0] * (M[9] * M[E] - M[A] * M[D]) -
-             M[8] * (M[1] * M[E] - M[2] * M[D]) +
-             M[C] * (M[1] * M[A] - M[2] * M[9]));
+    T[4] = -(mulf32(M[1], (mulf32(M[A], M[F]) - mulf32(M[B], M[E])) ) -
+             mulf32(M[9], (mulf32(M[2], M[F]) - mulf32(M[3], M[E])) ) +
+             mulf32(M[D], (mulf32(M[2], M[B]) - mulf32(M[3], M[A])) ));
+    T[5] = +(mulf32(M[0], (mulf32(M[A], M[F]) - mulf32(M[B], M[E])) ) -
+             mulf32(M[8], (mulf32(M[2], M[F]) - mulf32(M[3], M[E])) ) +
+             mulf32(M[C], (mulf32(M[2], M[B]) - mulf32(M[3], M[A])) ));
+    T[6] = -(mulf32(M[0], (mulf32(M[9], M[F]) - mulf32(M[B], M[D])) ) -
+             mulf32(M[8], (mulf32(M[1], M[F]) - mulf32(M[3], M[D])) ) +
+             mulf32(M[C], (mulf32(M[1], M[B]) - mulf32(M[3], M[9])) ));
+    T[7] = +(mulf32(M[0], (mulf32(M[9], M[E]) - mulf32(M[A], M[D])) ) -
+             mulf32(M[8], (mulf32(M[1], M[E]) - mulf32(M[2], M[D])) ) +
+             mulf32(M[C], (mulf32(M[1], M[A]) - mulf32(M[2], M[9])) ));
 
-    T[8] = +(M[1] * (M[6] * M[F] - M[7] * M[E]) -
-             M[5] * (M[2] * M[F] - M[3] * M[E]) +
-             M[D] * (M[2] * M[7] - M[3] * M[6]));
-    T[9] = -(M[0] * (M[6] * M[F] - M[7] * M[E]) -
-             M[4] * (M[2] * M[F] - M[3] * M[E]) +
-             M[C] * (M[2] * M[7] - M[3] * M[6]));
-    T[A] = +(M[0] * (M[5] * M[F] - M[7] * M[D]) -
-             M[4] * (M[1] * M[F] - M[3] * M[D]) +
-             M[C] * (M[1] * M[7] - M[3] * M[5]));
-    T[B] = -(M[0] * (M[5] * M[E] - M[6] * M[D]) -
-             M[4] * (M[1] * M[E] - M[2] * M[D]) +
-             M[C] * (M[1] * M[6] - M[2] * M[5]));
+    T[8] = +(mulf32(M[1], (mulf32(M[6], M[F]) - mulf32(M[7], M[E])) ) -
+             mulf32(M[5], (mulf32(M[2], M[F]) - mulf32(M[3], M[E])) ) +
+             mulf32(M[D], (mulf32(M[2], M[7]) - mulf32(M[3], M[6])) ));
+    T[9] = -(mulf32(M[0], (mulf32(M[6], M[F]) - mulf32(M[7], M[E])) ) -
+             mulf32(M[4], (mulf32(M[2], M[F]) - mulf32(M[3], M[E])) ) +
+             mulf32(M[C], (mulf32(M[2], M[7]) - mulf32(M[3], M[6])) ));
+    T[A] = +(mulf32(M[0], (mulf32(M[5], M[F]) - mulf32(M[7], M[D])) ) -
+             mulf32(M[4], (mulf32(M[1], M[F]) - mulf32(M[3], M[D])) ) +
+             mulf32(M[C], (mulf32(M[1], M[7]) - mulf32(M[3], M[5])) ));
+    T[B] = -(mulf32(M[0], (mulf32(M[5], M[E]) - mulf32(M[6], M[D])) ) -
+             mulf32(M[4], (mulf32(M[1], M[E]) - mulf32(M[2], M[D])) ) +
+             mulf32(M[C], (mulf32(M[1], M[6]) - mulf32(M[2], M[5])) ));
 
-    T[C] = -(M[1] * (M[6] * M[B] - M[7] * M[A]) -
-             M[5] * (M[2] * M[B] - M[3] * M[A]) +
-             M[9] * (M[2] * M[7] - M[3] * M[6]));
-    T[D] = +(M[0] * (M[6] * M[B] - M[7] * M[A]) -
-             M[4] * (M[2] * M[B] - M[3] * M[A]) +
-             M[8] * (M[2] * M[7] - M[3] * M[6]));
-    T[E] = -(M[0] * (M[5] * M[B] - M[7] * M[9]) -
-             M[4] * (M[1] * M[B] - M[3] * M[9]) +
-             M[8] * (M[1] * M[7] - M[3] * M[5]));
-    T[F] = +(M[0] * (M[5] * M[A] - M[6] * M[9]) -
-             M[4] * (M[1] * M[A] - M[2] * M[9]) +
-             M[8] * (M[1] * M[6] - M[2] * M[5]));
+    T[C] = -(mulf32(M[1], (mulf32(M[6], M[B]) - mulf32(M[7], M[A])) ) -
+             mulf32(M[5], (mulf32(M[2]. M[B]) - mulf32(M[3], M[A])) ) +
+             mulf32(M[9], (mulf32(M[2], M[7]) - mulf32(M[3], M[6])) ));
+    T[D] = +(mulf32(M[0], (mulf32(M[6], M[B]) - mulf32(M[7], M[A])) ) -
+             mulf32(M[4], (mulf32(M[2], M[B]) - mulf32(M[3], M[A])) ) +
+             mulf32(M[8], (mulf32(M[2], M[7]) - mulf32(M[3], M[6])) ));
+    T[E] = -(mulf32(M[0], (mulf32(M[5], M[B]) - mulf32(M[7], M[9])) ) -
+             mulf32(M[4], (mulf32(M[1], M[B]) - mulf32(M[3], M[9])) ) +
+             mulf32(M[8], (mulf32(M[1], M[7]) - mulf32(M[3], M[5])) ));
+    T[F] = +(mulf32(M[0], (mulf32(M[5], M[A]) - mulf32(M[6], M[9])) ) -
+             mulf32(M[4], (mulf32(M[1], M[A]) - mulf32(M[2], M[9])) ) +
+             mulf32(M[8], (mulf32(M[1], M[6]) - mulf32(M[2], M[5])) ));
 
-    d = M[0] * T[0] + M[4] * T[4] + M[8] * T[8] + M[C] * T[C];
+    d = mulf32(M[0], T[0]) + mulf32(M[4], T[4]) + mulf32(M[8], T[8]) + mulf32(M[C], T[C]);
 
     int d_abs = (d < 0) ? -d : d;
     if (d_abs > TINY)
@@ -196,25 +196,25 @@ void mf_rot(int *M, const int *v, int a)
 
     vf_nrm(u, v);
 
-    U[0] = u[0] * u[0]; U[4] = u[0] * u[1]; U[8] = u[0] * u[2]; 
-    U[1] = u[1] * u[0]; U[5] = u[1] * u[1]; U[9] = u[1] * u[2]; 
-    U[2] = u[2] * u[0]; U[6] = u[2] * u[1]; U[A] = u[2] * u[2]; 
+    U[0] = mulf32(u[0], u[0]); U[4] = mulf32(u[0], u[1]); U[8] = mulf32(u[0], u[2]); 
+    U[1] = mulf32(u[1], u[0]); U[5] = mulf32(u[1], u[1]); U[9] = mulf32(u[1], u[2]); 
+    U[2] = mulf32(u[2], u[0]); U[6] = mulf32(u[2], u[1]); U[A] = mulf32(u[2], u[2]); 
 
     S[0] =     0; S[4] = -u[2]; S[8] =  u[1];
     S[1] =  u[2]; S[5] =     0; S[9] = -u[0];
     S[2] = -u[1]; S[6] =  u[0]; S[A] =     0;
 
-    M[0] = U[0] + c * (4096 - U[0]) + s * S[0];
-    M[1] = U[1] + c * (   0 - U[1]) + s * S[1];
-    M[2] = U[2] + c * (   0 - U[2]) + s * S[2];
+    M[0] = U[0] + mulf32(c, (4096 - U[0])) + mulf32(s, S[0]);
+    M[1] = U[1] + mulf32(c, (   0 - U[1])) + mulf32(s, S[1]);
+    M[2] = U[2] + mulf32(c, (   0 - U[2])) + mulf32(s, S[2]);
     M[3] = 0;
-    M[4] = U[4] + c * (   0 - U[4]) + s * S[4];
-    M[5] = U[5] + c * (4096 - U[5]) + s * S[5];
-    M[6] = U[6] + c * (   0 - U[6]) + s * S[6];
+    M[4] = U[4] + mulf32(c, (   0 - U[4])) + mulf32(s, S[4]);
+    M[5] = U[5] + mulf32(c, (4096 - U[5])) + mulf32(s, S[5]);
+    M[6] = U[6] + mulf32(c, (   0 - U[6])) + mulf32(s, S[6]);
     M[7] = 0;
-    M[8] = U[8] + c * (   0 - U[8]) + s * S[8];
-    M[9] = U[9] + c * (   0 - U[9]) + s * S[9];
-    M[A] = U[A] + c * (4096 - U[A]) + s * S[A];
+    M[8] = U[8] + mulf32(c, (   0 - U[8])) + mulf32(s, S[8]);
+    M[9] = U[9] + mulf32(c, (   0 - U[9])) + mulf32(s, S[9]);
+    M[A] = U[A] + mulf32(c, (4096 - U[A])) + mulf32(s, S[A]);
     M[B] = 0;
     M[C] = 0;
     M[D] = 0;
@@ -226,40 +226,40 @@ void mf_rot(int *M, const int *v, int a)
 
 void mf_mult(int *M, const int *N, const int *O)
 {
-    M[0] = N[0] * O[0] + N[4] * O[1] + N[8] * O[2] + N[C] * O[3];
-    M[1] = N[1] * O[0] + N[5] * O[1] + N[9] * O[2] + N[D] * O[3];
-    M[2] = N[2] * O[0] + N[6] * O[1] + N[A] * O[2] + N[E] * O[3];
-    M[3] = N[3] * O[0] + N[7] * O[1] + N[B] * O[2] + N[F] * O[3];
+    M[0] = mulf32(N[0], O[0]) + mulf32(N[4], O[1]) + mulf32(N[8], O[2]) + mulf32(N[C], O[3]);
+    M[1] = mulf32(N[1], O[0]) + mulf32(N[5], O[1]) + mulf32(N[9], O[2]) + mulf32(N[D], O[3]);
+    M[2] = mulf32(N[2], O[0]) + mulf32(N[6], O[1]) + mulf32(N[A], O[2]) + mulf32(N[E], O[3]);
+    M[3] = mulf32(N[3], O[0]) + mulf32(N[7], O[1]) + mulf32(N[B], O[2]) + mulf32(N[F], O[3]);
 
-    M[4] = N[0] * O[4] + N[4] * O[5] + N[8] * O[6] + N[C] * O[7];
-    M[5] = N[1] * O[4] + N[5] * O[5] + N[9] * O[6] + N[D] * O[7];
-    M[6] = N[2] * O[4] + N[6] * O[5] + N[A] * O[6] + N[E] * O[7];
-    M[7] = N[3] * O[4] + N[7] * O[5] + N[B] * O[6] + N[F] * O[7];
+    M[4] = mulf32(N[0], O[4]) + mulf32(N[4], O[5]) + mulf32(N[8], O[6]) + mulf32(N[C], O[7]);
+    M[5] = mulf32(N[1], O[4]) + mulf32(N[5], O[5]) + mulf32(N[9], O[6]) + mulf32(N[D], O[7]);
+    M[6] = mulf32(N[2], O[4]) + mulf32(N[6], O[5]) + mulf32(N[A], O[6]) + mulf32(N[E], O[7]);
+    M[7] = mulf32(N[3], O[4]) + mulf32(N[7], O[5]) + mulf32(N[B], O[6]) + mulf32(N[F], O[7]);
 
-    M[8] = N[0] * O[8] + N[4] * O[9] + N[8] * O[A] + N[C] * O[B];
-    M[9] = N[1] * O[8] + N[5] * O[9] + N[9] * O[A] + N[D] * O[B];
-    M[A] = N[2] * O[8] + N[6] * O[9] + N[A] * O[A] + N[E] * O[B];
-    M[B] = N[3] * O[8] + N[7] * O[9] + N[B] * O[A] + N[F] * O[B];
+    M[8] = mulf32(N[0], O[8]) + mulf32(N[4], O[9]) + mulf32(N[8], O[A]) + mulf32(N[C], O[B]);
+    M[9] = mulf32(N[1], O[8]) + mulf32(N[5], O[9]) + mulf32(N[9], O[A]) + mulf32(N[D], O[B]);
+    M[A] = mulf32(N[2], O[8]) + mulf32(N[6], O[9]) + mulf32(N[A], O[A]) + mulf32(N[E], O[B]);
+    M[B] = mulf32(N[3], O[8]) + mulf32(N[7], O[9]) + mulf32(N[B], O[A]) + mulf32(N[F], O[B]);
 
-    M[C] = N[0] * O[C] + N[4] * O[D] + N[8] * O[E] + N[C] * O[F];
-    M[D] = N[1] * O[C] + N[5] * O[D] + N[9] * O[E] + N[D] * O[F];
-    M[E] = N[2] * O[C] + N[6] * O[D] + N[A] * O[E] + N[E] * O[F];
-    M[F] = N[3] * O[C] + N[7] * O[D] + N[B] * O[E] + N[F] * O[F];
+    M[C] = mulf32(N[0], O[C]) + mulf32(N[4], O[D]) + mulf32(N[8], O[E]) + mulf32(N[C], O[F]);
+    M[D] = mulf32(N[1], O[C]) + mulf32(N[5], O[D]) + mulf32(N[9], O[E]) + mulf32(N[D], O[F]);
+    M[E] = mulf32(N[2], O[C]) + mulf32(N[6], O[D]) + mulf32(N[A], O[E]) + mulf32(N[E], O[F]);
+    M[F] = mulf32(N[3], O[C]) + mulf32(N[7], O[D]) + mulf32(N[B], O[E]) + mulf32(N[F], O[F]);
 }
 
 void mf_pxfm(int *v, const int *M, const int *w)
 {
-    int d = w[0] * M[3] + w[1] * M[7] + w[2] * M[B] + M[F];
+    int d = mulf32(w[0], M[3]) + mulf32(w[1], M[7]) + mulf32(w[2], M[B]) + M[F];
 
-    v[0] = divf32((w[0] * M[0] + w[1] * M[4] + w[2] * M[8] + M[C]), d);
-    v[1] = divf32((w[0] * M[1] + w[1] * M[5] + w[2] * M[9] + M[D]), d);
-    v[2] = divf32((w[0] * M[2] + w[1] * M[6] + w[2] * M[A] + M[E]), d);
+    v[0] = divf32((mulf32(w[0], M[0]) + mulf32(w[1], M[4]) + mulf32(w[2], M[8]) + M[C]), d);
+    v[1] = divf32((mulf32(w[0], M[1]) + mulf32(w[1], M[5]) + mulf32(w[2], M[9]) + M[D]), d);
+    v[2] = divf32((mulf32(w[0], M[2]) + mulf32(w[1], M[6]) + mulf32(w[2], M[A]) + M[E]), d);
 }
 
 void mf_vxfm(int *v, const int *M, const int *w)
 {
-    v[0] = (w[0] * M[0] + w[1] * M[4] + w[2] * M[8]);
-    v[1] = (w[0] * M[1] + w[1] * M[5] + w[2] * M[9]);
-    v[2] = (w[0] * M[2] + w[1] * M[6] + w[2] * M[A]);
+    v[0] = (mulf32(w[0], M[0]) + mulf32(w[1], M[4]) + mulf32(w[2], M[8]));
+    v[1] = (mulf32(w[0], M[1]) + mulf32(w[1], M[5]) + mulf32(w[2], M[9]));
+    v[2] = (mulf32(w[0], M[2]) + mulf32(w[1], M[6]) + mulf32(w[2], M[A]));
 }
 
